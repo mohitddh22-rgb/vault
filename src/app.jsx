f27547749b11c90bc0404358301dfff1d0bab50b
mohitddh22-rgb <mohitddh22@gmail.com>
2025-09-27 18:11:59 +0530

    comit

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ color: "white", background: "black", height: "100vh" }}>
+      <h1>🚀 Vault App is Running!</h1>
+      <p>If you see this, your setup is working fine.</p>
+    </div>
+  );
+}
