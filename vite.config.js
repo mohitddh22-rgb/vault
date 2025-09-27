9a99744393c527d37a2c3d865eb9a0bdb66d1e5e
mohitddh22-rgb <mohitddh22@gmail.com>
2025-09-27 18:41:10 +0530

    vite file added

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  // Use this ONLY if deploying under a subpath (e.g. https://example.com/vault-app/)
+  base: "/vault/"
+});
